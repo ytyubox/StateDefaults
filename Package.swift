f0fdacd3435f534c049f733c6581383cd5ee2479
Tsungyu Yu <ytyubox@gmail.com>
2020-04-13 20:45:24 +0800

    update platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "StateDefaults",
-		platforms: [.iOS(SupportedPlatform.IOSVersion.v13)],
+		platforms: [
+			.iOS(SupportedPlatform.IOSVersion.v13),
+			.macOS(SupportedPlatform.MacOSVersion.v10_15),
+	],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
